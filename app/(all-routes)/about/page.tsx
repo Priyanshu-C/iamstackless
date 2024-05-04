c383ea6a75d1b90b4699c61609b370036054afae
Priyanshu Chauhan <63398163+Priyanshu-C@users.noreply.github.com>
2024-05-04 14:34:17 +0530

    Remove SecondFold component from About page

--- app/(all-routes)/about/page.tsx
+++ app/(all-routes)/about/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import SecondFold from "../../../components/SecondFold";
 
 const page = () => {
     return (
         <div className="flex min-h-screen flex-col items-start justify-center">
             <h1 className="text-4xl font-bold text-center pl-8">About Page</h1>
-            <SecondFold />
         </div>
     );
 };
